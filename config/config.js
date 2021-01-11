7f05827a6ebf9b521b27952a263818d164cb4223
Ted Fossum <theodorefossum@gmail.com>
2021-01-11 12:09:42 -0500

    Deploy configs.

--- config/config.js
+++ config/config.js
@@ -1,19 +1,19 @@
 require('dotenv').config()
 
 module.exports = {
-  development: {
-   database: buildsightpro_development,
-    dialect: postgres
+  "development": {
+   "database": "buildsightpro_development",
+    "dialect": "postgres"
   },
-  test: {
-    database: buildsightpro_test,
-    dialect: postgres
+  "test": {
+    "database": "buildsightpro_test",
+    "dialect": "postgres"
   },
-  production: {
-    username: process.env.RDS_USERNAME,
-    password: process.env.RDS_PASSWORD,
-    database: process.env.RDS_DB_NAME,
-    host: process.env.RDS_HOSTNAME,
-    dialect: 'postgres'
+  "production": {
+    "username": process.env.RDS_USERNAME,
+    "password": process.env.RDS_PASSWORD,
+    "database": process.env.RDS_DB_NAME,
+    "host": process.env.RDS_HOSTNAME,
+    "dialect": 'postgres'
   }
 }
