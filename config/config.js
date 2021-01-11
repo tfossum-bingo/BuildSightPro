558967d1c8d21f4a65ef5c67ed752d973f734f2f
Ted Fossum <theodorefossum@gmail.com>
2021-01-11 16:14:02 -0500

    Changed prod config.

--- config/config.js
+++ config/config.js
@@ -9,11 +9,11 @@ module.exports = {
     "database": "buildsightpro_test",
     "dialect": "postgres"
   },
-  "production": {
-    "username": process.env.RDS_USERNAME,
-    "password": process.env.RDS_PASSWORD,
-    "database": process.env.RDS_DB_NAME,
-    "host": process.env.RDS_HOSTNAME,
-    "dialect": 'postgres'
+  production: {
+    username: process.env.RDS_USERNAME,
+    password: process.env.RDS_PASSWORD,
+    database: process.env.RDS_DB_NAME,
+    host: process.env.RDS_HOSTNAME,
+    dialect: 'postgres'
   }
 }
