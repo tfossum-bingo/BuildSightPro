505b5b9c9bea2d6ec9ab6101ab757d2daa94fa53
Ted Fossum <theodorefossum@gmail.com>
2021-02-03 21:34:42 -0500

    Jobsites page pre-loads company user options eliminating form flash and disappear.

--- client/src/pages/JobsitesPage.js
+++ client/src/pages/JobsitesPage.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import { connect } from 'react-redux'
+import {connect} from 'react-redux'
 import { getUser } from '../store/actions/UserActions'
 import { setJobsitesNeedsRefresh } from '../store/actions/JobsiteActions'
-
+import { populateCompanyUserOptions } from '../store/actions/CompanyActions'
 
 import Header from '../components/Header'
 import JobsiteList from '../components/jobsites/JobsitesList'
@@ -14,6 +14,12 @@ import Modal from '../components/modals/Modal'
 
 const JobsitesPage = (props) => {
     const [displayJobsiteModal, setDisplayModal] = useState(false)
+
+    useEffect(() => {
+        if(props.companyState.companyUserOptions.length === 0) {
+            props.populateCompanyUserOptions(props.userState.user.companyId)
+        }
+    }, [])
 
     const toggleJobsiteModal = () => {
         setDisplayModal(!displayJobsiteModal)
@@ -48,14 +54,16 @@ const JobsitesPage = (props) => {
 const mapActionsToProps = (dispatch) => {
     return {
         getUser: () => dispatch(getUser()),
+        populateCompanyUserOptions: (companyId) => dispatch(populateCompanyUserOptions(companyId)),
         setJobsitesNeedsRefresh: () => dispatch(setJobsitesNeedsRefresh())
     }
 }
 
 const mapStateToProps = (state) => {
     return {
-        userState: state.userState,
-        jobsiteState: state.JobsiteReducer
+        companyState: state.companyState,
+        jobsiteState: state.JobsiteReducer,
+        userState: state.userState
     }
 }
 
