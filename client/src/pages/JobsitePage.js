57b46d62af856882a127e1098f5a15d724efb3eb
Ted Fossum <theodorefossum@gmail.com>
2021-02-04 22:04:42 -0500

    JobsitePage will reload state upon refresh.

--- client/src/pages/JobsitePage.js
+++ client/src/pages/JobsitePage.js
@@ -1,8 +1,8 @@
-import React, { useEffect } from 'react';
-import { connect } from 'react-redux'
-import { NavLink } from 'react-router-dom'
+import React, {useEffect} from 'react';
+import {connect} from 'react-redux'
+import {NavLink} from 'react-router-dom'
 
-import { getJobsite, showJobsiteUserForm } from '../store/actions/JobsiteActions'
+import {getJobsite, showJobsiteUserForm} from '../store/actions/JobsiteActions'
 
 import JobsiteMeta from '../components/jobsites/JobsiteMeta'
 import JobsiteUsers from '../components/jobsite_users/JobsiteUsers'
@@ -16,49 +16,55 @@ const JobsitePage = (props) => {
     const jobsite_id = parseInt(props.match.params.jobsite_id)
 
     useEffect(() => {
-        if (props.jobsiteState.refreshJobsite || (props.jobsiteState.jobsite !== null && props.jobsiteState.jobsite.id !== jobsite_id)) {
+        if (jobsiteRefreshable()) {
             props.getJobsite(jobsite_id)
         }
     }, [props.jobsiteState.refreshJobsite])
 
+    const jobsiteRefreshable = () => {
+        return props.jobsiteState.refreshJobsite || (props.jobsiteState.jobsite !== null && props.jobsiteState.jobsite.id !== jobsite_id)
+    }
 
-    if (props.jobsiteState.jobsite !== null && props.jobsiteState.jobsite !== undefined) {
-        return (
-            <div className={'flex-column'}>
-                <Header />
-                <div className='jobsite-main'>
-                    <div className='flex-row flex-justify-start' id='all-jobsites-link'>
-                        <NavLink to='/jobsites'>
-                            - Return to All Jobsites
-                        </NavLink>
+    const pageLoading = () => {
+        return props.jobsiteState.jobsite !== null && props.jobsiteState.jobsite !== undefined
+    }
+
+    return pageLoading() ? (
+        <div className={'flex-column'}>
+            <Header/>
+            <div className='jobsite-main'>
+                <div className='flex-row flex-justify-start' id='all-jobsites-link'>
+                    <NavLink to='/jobsites'>
+                        - Return to All Jobsites
+                    </NavLink>
+                </div>
+                <div className='jobsite-address-container'>
+                    <div className='map-container'>
+                        <SimpleMap center={{
+                            lat: props.jobsiteState.jobsite.latitude,
+                            lng: props.jobsiteState.jobsite.longitude
+                        }} zoom={15}/>
                     </div>
-                    <div className='jobsite-address-container'>
-                        <div className='map-container'>
-                            <SimpleMap center={{ lat: props.jobsiteState.jobsite.latitude, lng: props.jobsiteState.jobsite.longitude }} zoom={15} />
-                        </div>
-                        <div className='address-container'>
-                            <JobsiteMeta jobsite={props.jobsiteState.jobsite} />
-                        </div>
-                        <div className='jobsiteuser-container'>
-                            <JobsiteUsers />
-                        </div>
+                    <div className='address-container'>
+                        <JobsiteMeta jobsite={props.jobsiteState.jobsite}/>
                     </div>
-                    <div>
-                        <Modal show={props.jobsiteState.displayJobsiteUserForm}>
-                            <JobsiteUserForm />
-                        </Modal>
-                    </div>
-                    <div>
-                        <Specifications />
+                    <div className='jobsiteuser-container'>
+                        <JobsiteUsers/>
                     </div>
                 </div>
+                <div>
+                    <Modal show={props.jobsiteState.displayJobsiteUserForm}>
+                        <JobsiteUserForm/>
+                    </Modal>
+                </div>
+                <div>
+                    <Specifications/>
+                </div>
             </div>
-        )
-    } else {
-        return (
-            <div>Loading...</div>
-        )
-    }
+        </div>
+    ) : (
+        <div>Loading...</div>
+    )
 }
 
 
