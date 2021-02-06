c2717cce9e92ca1c56ae6188ddca1a0f409149e4
Ted Fossum <theodorefossum@gmail.com>
2021-02-06 12:57:59 -0500

    Changed prod configs.

--- config/config.js
+++ config/config.js
@@ -2,8 +2,13 @@ require('dotenv').config()
 
 module.exports = {
   development: {
-   database: "buildsightpro_development",
-    dialect: "postgres"
+   // database: "buildsightpro_development",
+   //  dialect: "postgres"
+    username: process.env.RDS_USERNAME,
+    password: process.env.RDS_PASSWORD,
+    database: process.env.RDS_DB_NAME,
+    host: process.env.RDS_HOSTNAME,
+    dialect: 'mysql'
   },
   test: {
     database: "buildsightpro_test",
@@ -14,6 +19,6 @@ module.exports = {
     password: process.env.RDS_PASSWORD,
     database: process.env.RDS_DB_NAME,
     host: process.env.RDS_HOSTNAME,
-    dialect: 'postgres'
+    dialect: 'mysql'
   }
 }
