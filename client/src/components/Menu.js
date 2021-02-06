b107a094cc14960337aeefb9f4c7a06050d14101
Ted Fossum <theodorefossum@gmail.com>
2021-02-05 23:14:21 -0500

    Added company name to menu.

--- client/src/components/Menu.js
+++ client/src/components/Menu.js
@@ -2,29 +2,34 @@ import React from 'react'
 import SignOut from './SignOut'
 
 const Menu = (props) => {
-    const { firstName, lastName, email } = props.user
+    const {Company, firstName, lastName, email} = props.user
 
     if (props.displayMenu) {
         return (
             <div className='menu-pop-out modal-styles'>
-                <div>
-                    <button
-                        className='modal-close-button'
-                        onClick={props.onClick}>
-                        X
-                    </button>
+                <div className="menu-close-container">
+                    <div>
+                        <button
+                            className='modal-close-button'
+                            onClick={props.onClick}>
+                            X
+                        </button>
+                    </div>
                 </div>
-                <div>
+                <div className="menu-text">
                     user: {firstName + " " + lastName}
                 </div>
-                <div>
+                <div className="menu-text">
+                    company: {Company.name}
+                </div>
+                <div className="menu-text">
                     email: {email}
                 </div>
                 <div>
                     {/* org: {organization_id.name}  */}
                 </div>
                 <div className='menu-logout'>
-                    <SignOut />
+                    <SignOut/>
                 </div>
             </div>
         )
