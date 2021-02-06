e208d9480b639ab93eef32a18953bf3e98629360
Ted Fossum <theodorefossum@gmail.com>
2021-02-05 20:33:03 -0500

    Refresh data icon now spins signalling refresh.

--- client/src/components/ListHeader.js
+++ client/src/components/ListHeader.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faPlusSquare, faSyncAlt } from "@fortawesome/free-solid-svg-icons"
+// const baseSpinnerClasses = ['fas', 'fa-white', 'list-header-icons']
 
 const ListHeader = (props) => {
     // const {addForm, refreshAction} = props
+    const [synchIconClasses, setSynchIconClasses] = useState(['list-header-sync', 'list-header-icons-containers'])
     const { displayFormAction, refreshAction, title, width } = props
     const appliedWidth = width ? width : '300px'
+
+    const handleRefreshClick = () => {
+        setSynchIconClasses(['list-header-sync', 'list-header-icons-containers', 'synch-spinner'])
+        setTimeout(async function(){
+            await refreshAction()
+            setSynchIconClasses(['list-header-sync', 'list-header-icons-containers'])
+        }, 500);
+    }
+
     return (
         <div className='list-header-container' style={{ width: appliedWidth }}>
             <div className='list-header-add list-header-icons-containers'>
@@ -19,9 +30,11 @@ const ListHeader = (props) => {
             <div className='list-header-title'>
                 {title ? title : `Title`}
             </div>
-            <div className='list-header-sync list-header-icons-containers'>
+            <div className={synchIconClasses.join(" ")}>
+            {/*<div className='list-header-sync list-header-icons-containers synch-spinner'>*/}
                 <FontAwesomeIcon
-                    onClick={refreshAction}
+                    onClick={handleRefreshClick}
+                    // className={synchIconClasses.join(" ")}
                     className="fas fa-white list-header-icons"
                     style={{ color: "white" }}
                     icon={faSyncAlt}
