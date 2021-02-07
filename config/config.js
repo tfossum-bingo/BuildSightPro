c7a7db8228540f80973dfda42d1efa3ca2f6272b
Ted Fossum <theodorefossum@gmail.com>
2021-02-06 19:21:46 -0500

    Changed prod configs.

--- config/config.js
+++ config/config.js
@@ -18,6 +18,7 @@ module.exports = {
     username: process.env.RDS_USERNAME,
     password: process.env.RDS_PASSWORD,
     database: process.env.RDS_DB_NAME,
+    port: process.env.RDS_PORT,
     host: process.env.RDS_HOSTNAME,
     dialect: 'mysql'
   }
