6f1ad252551aa3eb89f9f98ec0ee683f1bb55f10
Ted Fossum <theodorefossum@gmail.com>
2021-02-12 20:09:01 -0500

    Fixed issue with jobsite page layout.

--- config/config.js
+++ config/config.js
@@ -1,25 +1,25 @@
 require('dotenv').config()
 
 module.exports = {
-  development: {
-   // database: "buildsightpro_development",
-   //  dialect: "postgres"
-    username: process.env.RDS_USERNAME,
-    password: process.env.RDS_PASSWORD,
-    database: process.env.RDS_DB_NAME,
-    host: process.env.RDS_HOSTNAME,
-    dialect: 'mysql'
-  },
-  test: {
-    database: "buildsightpro_test",
-    dialect: "postgres"
-  },
-  production: {
-    username: process.env.RDS_USERNAME,
-    password: process.env.RDS_PASSWORD,
-    database: process.env.RDS_DB_NAME,
-    port: process.env.RDS_PORT,
-    host: process.env.RDS_HOSTNAME,
-    dialect: 'mysql'
-  }
+    development: {
+        database: "buildsightpro_development",
+        dialect: "postgres"
+        //  username: process.env.RDS_USERNAME,
+        //  password: process.env.RDS_PASSWORD,
+        //  database: process.env.RDS_DB_NAME,
+        //  host: process.env.RDS_HOSTNAME,
+        //  dialect: 'mysql'
+    },
+    test: {
+        database: "buildsightpro_test",
+        dialect: "postgres"
+    },
+    production: {
+        username: process.env.RDS_USERNAME,
+        password: process.env.RDS_PASSWORD,
+        database: process.env.RDS_DB_NAME,
+        port: process.env.RDS_PORT,
+        host: process.env.RDS_HOSTNAME,
+        dialect: 'mysql'
+    }
 }
