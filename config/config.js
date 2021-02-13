a52024a307623a9cca574d6fbbaaa9e8499ce624
Ted Fossum <theodorefossum@gmail.com>
2021-02-13 13:27:28 -0500

    New db config to shared server on aws.

--- config/config.js
+++ config/config.js
@@ -2,13 +2,13 @@ require('dotenv').config()
 
 module.exports = {
     development: {
-        database: "buildsightpro_development",
-        dialect: "postgres"
-        //  username: process.env.RDS_USERNAME,
-        //  password: process.env.RDS_PASSWORD,
-        //  database: process.env.RDS_DB_NAME,
-        //  host: process.env.RDS_HOSTNAME,
-        //  dialect: 'mysql'
+        // database: "buildsightpro_development",
+        // dialect: "postgres"
+         username: process.env.RDS_USERNAME,
+         password: process.env.RDS_PASSWORD,
+         database: process.env.RDS_DB_NAME,
+         host: process.env.RDS_HOSTNAME,
+         dialect: 'mysql'
     },
     test: {
         database: "buildsightpro_test",
